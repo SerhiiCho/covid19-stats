8972c241ad11fc07320bdc8cd6c501cf3e7b08e9
SerhiiCho <serhiicho@protonmail.com>
2020-04-03 13:45:35 +0300

    Added table to Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,29 @@
 <template>
-    <div>Hello world</div>
+    <div>
+        <h5>World COVID-19 statistics</h5>
+
+        <table class="striped highlight">
+            <thead>
+                <tr>
+                    <th>Country</th>
+                    <th>Confirmed</th>
+                    <th>New cases</th>
+                    <th>Deceased</th>
+                    <th>Recovered</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>Jonathan</td>
+                    <td>Lollipop</td>
+                    <td>$7.00</td>
+                    <td>Lollipop</td>
+                    <td>$7.00</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
