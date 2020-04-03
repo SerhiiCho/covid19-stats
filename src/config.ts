d73b6ecb56aa6536e0fa8e336f2264e4eb95c36b
SerhiiCho <serhiicho@protonmail.com>
2020-04-03 18:07:10 +0300

    added config.ts file

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+const config: { [P: string]: string } = {
+    requestHost: "covid-193.p.rapidapi.com",
+    requestURL: "https://covid-193.p.rapidapi.com/statistics",
+    requestKey: "091b42d8ecmsh380ec626aa86af5p14908ajsne8ee54be4633",
+}
+
+export default config
