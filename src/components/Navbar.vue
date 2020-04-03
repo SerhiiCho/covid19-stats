66c5cd3e1b30b8bf7d04ffff483f45689becb19c
SerhiiCho <serhiicho@protonmail.com>
2020-04-03 13:45:07 +0300

    Added logo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,11 @@
 <template>
-    <nav class="grey darken-3">
+    <nav class="red darken-3">
         <div class="container">
             <div class="nav-wrapper">
-                <a href="#" class="brand-logo">COVID19</a>
+                <router-link class="brand-logo" to="/">
+                    C<img src="@/assets/covid.png" alt="covid logo" height="23">VID-19
+                </router-link>
+
                 <ul id="nav-mobile" class="right hide-on-med-and-down">
                     <router-link tag="li" to="/"><a>Home</a></router-link>
                 </ul>
