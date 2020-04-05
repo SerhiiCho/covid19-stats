4d7f229fea858ebef65411c34aedb349895045b1
SerhiiCho <serhiicho@protonmail.com>
2020-04-05 11:52:58 +0300

    Added sidenav

---
+++ src/components/NavLinks.vue
@@ -0,0 +1,24 @@
+<template>
+    <ul>
+        <router-link tag="li" to="/" exact>
+            <a>Dashboard</a>
+        </router-link>
+        <router-link tag="li" to="/what-to-do" exact>
+            <a>What to do?</a>
+        </router-link>
+    </ul>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+    
+})
+</script>
+
+<style lang="scss" scoped>
+    nav ul li.active {
+        background-color: rgba(0,0,0,0.4);
+    }
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,41 +1,78 @@
 <template>
-    <nav class="blue-grey darken-4">
-        <div class="container">
-            <div class="nav-wrapper">
-                <router-link class="brand-logo" to="/">
-                    C<img src="@/assets/covid.png" alt="covid logo" height="30" />VID-19
-                </router-link>
+    <div>
+        <nav class="blue-grey darken-4">
+            <div class="container">
+                <a href="javascript:" class="sidenav-trigger" @click="toggleSidenav">
+                    OO
+                </a>
 
-                <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <router-link tag="li" to="/" exact>
-                        <a>Dashboard</a>
+                <div class="nav-wrapper">
+                    <router-link class="brand-logo" to="/">
+                        C<img src="@/assets/covid.png" alt="covid logo" height="30" />VID-19
                     </router-link>
-                    <router-link tag="li" to="/what-to-do" exact>
-                        <a>What to do?</a>
-                    </router-link>
-                </ul>
+
+                    <div class="hide-on-med-and-down right"><NavLinks /></div>
+                </div>
             </div>
+        </nav>
+        
+        <div v-if="showSidebar">
+            <div class="sidenav-overlay" @click="toggleSidenav"></div>
         </div>
-    </nav>
+
+        <div v-if="showSidebar">
+            <ul class="sidenav">
+                <li>
+                    <div class="user-view">
+                        <div class="background">
+                            <img src="@/assets/img/covid19.jpg">
+                        </div>
+
+                        <a><span class="white-text name">COVID-19</span></a>
+                        <a><span class="white-text email">Coronavirus</span></a>
+                    </div>
+                </li>
+                <NavLinks />
+            </ul>
+        </div>
+    </div>
 </template>
 
-<script>
+<script lang="ts">
 import Vue from 'vue'
+import NavLinks from '@/components/NavLinks.vue'
 
 export default Vue.extend({
-   //
+    data(): {
+        showSidebar: boolean
+    } {
+        return {
+            showSidebar: false,
+        }
+    },
+
+    methods: {
+        toggleSidenav(): void {
+            this.showSidebar = !this.showSidebar
+        }
+    },
+
+    components: {
+        NavLinks,
+    },
 })
 </script>
 
 <style lang="scss" scoped>
-.brand-logo {
-    font-size: 1.6em;
+    .brand-logo {
+        font-size: 1.6em;
 
-    img {
-        transform: translateY(7px);
+        img {
+            transform: translateY(7px);
+        }
     }
-}
-nav ul li.active {
-    background-color: rgba(0,0,0,0.4);
-}
+
+    .sidenav .background img {
+        width: 100%;
+    }
 </style>
