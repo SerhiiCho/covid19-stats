5b5dfcd371a69060556ee8ccaa6f12b24f4334e4
SerhiiCho <serhiicho@protonmail.com>
2025-07-08 21:46:16 +0300

    fix: issues with typescript

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import WhatToDo from '../views/WhatToDo.vue'
+import Home from '@/views/Home.vue'
+import WhatToDo from '@/views/WhatToDo.vue'
 
 const routes = [
     {
